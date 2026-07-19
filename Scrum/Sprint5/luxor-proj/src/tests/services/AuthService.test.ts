8e74c014d74977815c554930f926f20f0f1252d5
JAVIERCLO <cha23132@uvg.edu.gt>
2026-07-18 21:15:28 -0600

    test de login para tarea 3

---
+++ Scrum/Sprint5/luxor-proj/src/tests/services/AuthService.test.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { login } from "../../validation/authService";
+
+describe("authService", () => {
+
+    beforeEach(() => {
+        vi.restoreAllMocks();
+    });
+
+// test de login exitoso
+    it("debe retornar el usuario cuando el login es exitoso", async () => {
+
+        const credentials = {
+        email: "admin@luxor.com",
+        password: "123456"
+        };
+
+        vi.spyOn(globalThis, "fetch").mockResolvedValue({
+        ok: true,
+        status: 200,
+        json: async () => ({
+            success: true,
+            user: {
+            id: 1,
+            name: "Administrador",
+            role: "ADMIN",
+            },
+        }),
+        } as Response);
+
+        const result = await login(credentials);
+
+        expect(result.success).toBe(true);
+
+        expect(result.user).toEqual({
+        id: 1,
+        name: "Administrador",
+        role: "ADMIN",
+        email: "admin@luxor.com",
+        });
+
+    });
+
+});
