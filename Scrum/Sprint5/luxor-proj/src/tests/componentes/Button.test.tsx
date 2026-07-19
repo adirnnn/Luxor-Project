ca6a85faf9d21f65837daff13ad9d7d59bcc492b
JAVIERCLO <cha23132@uvg.edu.gt>
2026-07-18 18:33:08 -0600

    prueba de click del componente button para tarea 3

--- Scrum/Sprint5/luxor-proj/src/tests/componentes/Button.test.tsx
+++ Scrum/Sprint5/luxor-proj/src/tests/componentes/Button.test.tsx
@@ -1,13 +1,33 @@
-// Test para verificar si el botón se renderiza correctamente
+// Pruebas del funcionamiento del componente Button
 
 import { render, screen } from "@testing-library/react";
 import { Button } from "../../components/ui/Button";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
 
+// Test de renderizado de button
 describe("Button", () => {
     it("debe renderizar el texto del botón", () => {
         render(<Button>Comprar</Button>);
 
         expect(screen.getByText("Comprar")).toBeInTheDocument();
     });
+
+// Test de funcionamiento de click del button
+    it("debe ejecutar onClick cuando se hace clic", async () => {
+    const handleClick = vi.fn();
+
+    render(
+        <Button onClick={handleClick}>
+        Comprar
+        </Button>
+    );
+
+    const button = screen.getByRole("button", { name: "Comprar" });
+
+    await userEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+    });
 });
+
